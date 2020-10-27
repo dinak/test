2d22bfa4757ceeffae2538d0e0aca54ceb189f83
dinak <dinesh.amatya@gmail.com>
2020-10-27 11:58:35 +0545

    Update itc.sh

--- itc.sh
+++ itc.sh
@@ -4,7 +4,5 @@ user=$1
 
 password=$2
 
-echo "Downloading itc rpm"
-
 curl -u $user:$password https://service.itconductor.com/lib/itconductor-gateway-6-65.x86_64.rpm --output ~/itconductor-gateway-6-65.x86_64.rpm
 
