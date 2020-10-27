2d6ddc5f9b3ad744e57e497f4fa2e43335e87a09
dinak <dinesh.amatya@gmail.com>
2020-10-27 12:00:14 +0545

    Update itc.sh

--- itc.sh
+++ itc.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-
 user=$1
-
 password=$2
-
-curl -u $user:$password https://service.itconductor.com/lib/itconductor-gateway-6-65.x86_64.rpm --output ~/itconductor-gateway-6-65.x86_64.rpm
-
+curl -u $user:$password https://service.itconductor.com/lib/itconductor-gateway-6-65.x86_64.rpm --output /home/itcadmin/itconductor-gateway-6-65.x86_64.rpm
