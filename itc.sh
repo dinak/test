1532b8458c2f67151dc1df0abb8fbea5bc300da2
dinak <dinesh.amatya@gmail.com>
2020-10-28 09:19:42 +0545

    Update itc.sh

--- itc.sh
+++ itc.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-user=$1
-password=$2
-curl -u $user:$password https://service.itconductor.com/lib/itconductor-gateway-6-65.x86_64.rpm --output /home/itcadmin/itconductor-gateway-6-65.x86_64.rpm
+accountId=$1
+apiKey=$2
+gatewayId=$3
+linuxUser=$4
+
+curl -u $accountId:$apiKey https://service.itconductor.com/lib/itconductor-gateway-6-65.x86_64.rpm --output /home/$linuxUser/itconductor-gateway-6-65.x86_64.rpm
 
 sudo apt-get update
 
-sudo rpm -U --nodeps  /home/itcadmin/itconductor-gateway-6-65.x86_64.rpm
+sudo rpm -U --nodeps  /home/$linuxUser/itconductor-gateway-6-65.x86_64.rpm
 
